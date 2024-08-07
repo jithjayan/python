9a6010600cb4f56fa663abe78c5d59bef27b0748
jithjayan <jithjayan9341@gmail.com>
2024-08-07 15:31:03 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,75 @@
+# print(10+5)
+# print(10-5)
+# print(10*5)
+# print(10/5)
+# print(10%4)
+# print(10//4)
+# print(10**2)
+# print('ha'*2)
+
+# a=10
+# print(a)
+# a+=5
+# print(a)
+# a-=5
+# print(a)
+# a/=5
+# print(a)
+# a*=2
+# print(a)
+# a%=4
+# print(a)
+# a//=4
+# print(a)
+# a**=2
+# print(a)
+
+# print(10==10)
+# print(10==5)
+# print(10!=2)
+# print(10!=10)
+# print(10>1)
+# print(10>20)
+# print(10<20)
+# print(10<1)
+# print(10>=10)
+# print(10>=1)
+# print(10>=20)
+# print(10<=10)
+# print(10<=12)
+# print(10<=1)
+
+# print(10==10 and 10==10)
+# print(10==10 and 10==1)
+# print(10==1 and 10==10)
+# print(10==20 and 10==1)
+# print(10==10 or 10==10)
+# print(10==1 or 10==10)
+# print(10==10 or 10==1)
+# print(10==1 or 10==2)
+# print(not(10==10))
+# print(not(10==1))
+
+print('l' in 'lion')
+print('l' in 'jazz')
+print('l' not in 'igloo')
+print('l' not in 'tiger')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
