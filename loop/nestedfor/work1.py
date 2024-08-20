3e70e3e169b73bd9f4a849ad8bb6c91d4296762f
jithjayan <jithjayan9341@gmail.com>
2024-08-20 09:09:39 +0530

    nested for loop

---
+++ loop/nestedfor/work1.py
@@ -0,0 +1,7 @@
+a=0
+for i in range(3):
+    for j in range(3):
+        print(a,end=" ")
+        a+=1
+        
+    print()
---
+++ loop/nestedfor/work3.py
@@ -0,0 +1,9 @@
+
+for i in range(4):
+    for j in range(3):
+    #    print(j+i,end="") 
+        if i%2 == 0:
+            print(j,end=" ")
+        else:
+            print(2-j,end=" ")
+    print()
