231ca5cc91ed79c615d71c8413913799c8007736
jithjayan <jithjayan9341@gmail.com>
2024-08-23 09:06:47 +0530

    list

---
+++ list/sample.py
@@ -0,0 +1,48 @@
+# l=[1,2,10,20,12,'abc',1]
+# print(l)
+# print(l[1])
+
+# if 10 in l:
+#     print("available")
+# else:
+#     print(" not available")
+
+# l[0]=7
+
+# for i in l:
+#     print(i)  
+
+# l=[]
+
+# l.append(10)
+# l.append('abc')
+# l.append([10,11,12])
+
+# l.extend([100,200,210])
+
+# l.insert(2,'def')
+# print(l)
+# print(l[2])
+
+
+# l=[10,20,30]
+
+# l.clear()
+
+# l.pop()
+# l.pop(1)
+
+# l.remove(10)
+# print(l)
+
+# l=[10,20,30,8,5]
+
+# print(l.index(20))
+
+# print(l.count(10))  
+
+# l.sort()
+# print(l)
+
+# l.reverse()
+# print(l)
