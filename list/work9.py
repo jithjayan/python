e135a27650b33b06b84a347589d68015e4672b20
jithjayan <jithjayan9341@gmail.com>
2024-08-29 09:09:07 +0530

    employe work finish

--- list/work9.py
+++ list/work9.py
@@ -22,12 +22,70 @@ while True:
             print(i)
     elif op==3:
         id=int(input("Enter the id"))
-        if id in i:
-            while True:
-                print('''    1.salry
-                  2.position
-                  3.exp''')
-                upd=int(input("enter the value"))
-                if upd==1:
-                    f=0
-                    for i in emp:
+        f=0
+        for i in emp:
+            if id in i:
+                while True:
+                    print('''        1.salary
+        2.position
+        3.experiance
+        4.exit''')
+                    
+                    op=int(input("Enter the input: "))
+                    if op==1:
+                        slry=int(input("enter salary"))
+                        i[4]=slry
+                        f=1
+                    elif op==2:
+                        pos=str(input("Enter the position"))
+                        i[5]=pos
+                        f=1
+                    elif op==3:
+                        exp=int(input("Enter the experiance"))
+                        i[6]=exp
+                        f==1
+                    elif op==4:
+                        break
+                    else:
+                        print("invalid input")   
+        if f==0:
+            print("id not found")
+    elif op==4:
+        id=int(input("Enter the id"))
+        f=0
+        for i in emp:
+            if id in i:
+                emp.remove(i)
+                f=1
+        if f==0:
+                print("id not found")
+    elif op==5:
+        id=int(input("enter the id"))
+        f=0
+        import datetime
+        for i in emp:
+            if id in i:
+                task=str(input("enter the task"))
+                date=datetime.datetime.now().strftime('%x')
+                i.append([task,date])
+                f=1
+        if f==0:
+                print("id not found")
+    elif op==6:
+        id=int(input("enter the id"))
+        f=0
+        for i in emp:
+            if id in i:
+                print(i)
+                f=1
+        if f==0:
+                print("id not found")
+    elif op==7:
+        break
+    else:
+        print("invalid input")
+
+
+            
+
+                 
