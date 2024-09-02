5dcb8ed31b1debc91528a88c0b3362a0e25c9e45
jithjayan <jithjayan9341@gmail.com>
2024-09-02 09:13:28 +0530

    dictionary

---
+++ dictionary/sample.py
@@ -0,0 +1,35 @@
+d={'name':'anu','age':20,'place':'ekm'}
+# print(d)
+# print(d['name'])
+# print(d['age'])
+# print(d['place'])
+# for i in d:
+#     print(i)  
+#     print(d[i])
+# d['age']=25
+# d['mark']=45
+# print(d)
+
+# if d['age']==20:
+#     print('available')
+# else:
+#     print('not available')
+
+# print(d.get('name'))
+# print(d.items())
+# print(d.values())
+# print(d.keys())
+# a=d.copy()
+# d['mark']=22
+# print(a)
+# print(d)  
+# d.pop('place')
+# d.popitem()
+# d.setdefault('mark')
+# print(d)
+# l=[11,12,13]
+# print(d.fromkeys(l))
+a=input('key')
+b=input('value')
+d[a]=b
+print(d)
