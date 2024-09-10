f2e4e4b410eebf54659370654daf4aed0fdad2c9
jithjayan <jithjayan9341@gmail.com>
2024-09-10 09:02:23 +0530

    function

---
+++ function.py/sample.py
@@ -0,0 +1,30 @@
+# def sample():
+#     print("welcome")
+#     print("welcome1")
+#     print("welcome2")
+# a=[1,2,3,4,5]
+# print(a)
+# sample()
+# a=[1,2,3,4,5]
+# print(a)
+# sample()
+
+
+# def sample():
+#     b=0
+#     print("number b=",b)
+#     print("nunber a=",a)
+# a=1
+# sample()
+# print("number a=",a) 
+# print("number b=",b)  
+
+
+# def sample():
+#     a=0
+#     print("nunber a=",a)
+# a=1
+# sample()
+# print("number a=",a) 
+
+
