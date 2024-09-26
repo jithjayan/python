2948ee750ef3956c9adc90cef9c21bcf79393bfb
jithjayan <jithjayan9341@gmail.com>
2024-09-26 09:15:55 +0530

    write

--- method/sample.py
+++ method/sample.py
@@ -4,7 +4,7 @@
 # f.write('python\n')
 
 
-f=open('demo.txt','r')
+# f=open('demo.txt','r')
 # a=f.read()
 # print(a)
 
@@ -38,11 +38,79 @@ f=open('demo.txt','r')
 #     print(b[::-1])
 
 
-a=f.readlines()
-l=len(a)
-f.seek(0)
-for i in range(l):
-    b=f.readline().strip()
-    c=len(b)
-    print(c)
+# a=f.readlines()
+# l=len(a)
+# f.seek(0)
+# for i in range(l):
+#     b=f.readline().strip()
+#     c=len(b)
+#     print(c)
     
+# l=f.readlines()
+# letter=0
+# f.seek(0)
+# for i in range(len(l)):
+#     b=f.readline().strip()
+#     for i in b:
+#         if i !=' ':
+#             letter+=1
+#     print(letter)
+
+
+# l=f.readlines()
+# letter=0
+# s=0
+# f.seek(0)
+# for i in range(len(l)):
+#     b=f.readline().strip()
+#     for i in b:
+#         if i !=' ':
+#             letter+=1
+#             if i.islower():
+#                 s+=1
+
+# print('small',s)
+# print('cap',letter-s)
+
+# l=f.readlines()
+# words=0
+# f.seek(0)
+# for i in range(len(l)):
+#     b=f.readline().strip()
+#     for i in b:
+#         if i==' ':
+#             words+=1
+# words+=1
+# print(words)
+
+        
+
+# l=f.readlines()
+# w=0
+# f.seek(0)
+# for i in range(len(l)):
+#     b=f.readline().strip()
+#     a=b.split(' ')
+#     for j in a:
+#         if j!='':
+#             w+=1
+# print(w)
+# f=open('demo.txt','w')
+# f.write('hello\n')
+# f.write('alliens\n')
+# f.write('welcome  '+'here')
+
+
+# f=open('demoo.txt','w')
+# f.write('hello\n')
+# f.write('alliens\n')
+# f.write('welcome  '+'here')
+
+f=open('demoo.txt','w')
+num=int(input("enter the number"))
+if num>0:
+    for i in range(1,11):
+        q=(num*i)
+        print(q)
+        f.write(str(q)+'\n')
+        
