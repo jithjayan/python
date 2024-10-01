22aa5da65ef9e23738d779780ebe04c4d56312b7
jithjayan <jithjayan9341@gmail.com>
2024-10-01 09:05:00 +0530

    inheritance

---
+++ oops/inheritance.py
@@ -0,0 +1,74 @@
+# class syn():
+#     def python(self):
+#         self.a=10
+#         print("python")
+#     def php(self):
+#         print("php")
+
+# class novavi(syn):
+#     def dm(self):
+#         print("dm works")
+#     def web_dev(self):
+#         print("web dev")
+
+# novavi_staff=novavi()
+# novavi_staff.python()
+# novavi_staff.php()
+# print(novavi_staff.a)
+
+# std1=syn()
+# std1.python()
+
+
+# class father:
+#     def shop(self):
+#         print("hotel")
+#     def car(self):
+#         print("polo gt")
+# class mother:
+#     def dress_shop(self):
+#         print("dress shop")
+# class child(father,mother):
+#     def bike(self):
+#         print("bike")
+# sonu=child()
+# sonu.shop()
+# sonu.car(),sonu.dress_shop()
+
+
+# class school:
+#     def notes(self):
+#         print("lecture notes")
+#     def online(self):
+#         print("online classes")
+
+# class tution:
+#     def tu_notes(self):
+#         print("tuition notes")
+#     def gu_notes(self):
+#         print("guide notes")
+
+# class student(school,tution):
+#     def text_notes(self):
+#         print("text refered notes")
+# harley=student()
+# harley.notes()
+# harley.online()
+# harley.tu_notes()
+
+# class KU:
+#     def exam(self):
+#         print("exam")
+#     def result(self):
+#         print("result")
+
+# class clg:
+#     def notes(self):
+#         print("lecture notes")
+# class std(KU,clg):
+#     def uniform(self):
+#         print("uniform")
+# harley=std()
+# harley.exam()
+# harley.result()
+# harley.notes()  
--- oops/sample.py
+++ oops/sample.py
@@ -12,28 +12,28 @@
 
 
 
-class bank:
-    def __init__(s):
-        s.name=input('name')
-        s.age=int(input('age'))
-        s.bal=0
+# class bank:
+#     def __init__(s):
+#         s.name=input('name')
+#         s.age=int(input('age'))
+#         s.bal=0
 
-    def deposite(self,amt):
-        self.bal+=amt
-        print('deposite done')
-    def withdraw(self,amt):
-        self.bal-=amt
-        print('withdraw')
-    def balance(self):
-        print('balance',self.bal)
-obj=bank()
-obj.deposite(5000)
-obj.balance()
-obj.withdraw(2000)
-obj.balance()
-print(obj.bal)
+#     def deposite(self,amt):
+#         self.bal+=amt
+#         print('deposite done')
+#     def withdraw(self,amt):
+#         self.bal-=amt
+#         print('withdraw')
+#     def balance(self):
+#         print('balance',self.bal)
+# obj=bank()
+# obj.deposite(5000)
+# obj.balance()
+# obj.withdraw(2000)
+# obj.balance()
+# print(obj.bal)
 
-obj1=bank()
-obj1.deposite(100)
-obj1.balance()
+# obj1=bank()
+# obj1.deposite(100)
+# obj1.balance()
 
