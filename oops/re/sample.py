36df01682d15564ec4aaa8b9e125b3dced25b1eb
jithjayan <jithjayan9341@gmail.com>
2024-10-04 09:31:38 +0530

    re

---
+++ oops/re/sample.py
@@ -0,0 +1,93 @@
+import re
+# a='welcome to all'
+# print(re.sub('to','TO',a))
+
+# a='welcome to all'
+# print(re.sub('too','TO',a))
+
+# a='welcome to all'
+# print(re.split(' ',a))
+
+# a='welcome to all'
+# print(re.findall('to',a))
+
+# a='welcome to all'
+# print(re.findall('toO',a))
+
+# a='wetolcome to all to to to to'
+# print(re.findall('to',a))
+
+# a='wetolcome to all to to to to'
+# print(re.search('to',a))
+
+# a='abcd'
+# print(re.search('a.',a)) 
+
+# a='abcd'
+# print(re.search('d.',a)) 
+
+# a='abcd'
+# print(re.search('a.*',a)) 
+
+# a='abcd'
+# print(re.search('d.*',a)) 
+
+# a='abcd'
+# print(re.search('a.+',a)) 
+
+# a='abcd'
+# print(re.search('c.+',a)) 
+
+# a='abcd'
+# print(re.search('d.+',a)) 
+
+# a='abcd'
+# print(re.search('a.?',a)) 
+
+# a='abcd'
+# print(re.search('c.?',a)) 
+
+# a='abcd'
+# print(re.search('d.?',a)) 
+
+# a='abcd'
+# print(re.search('[a-z]',a)) 
+
+# a='ABCD'
+# print(re.search('[a-z]',a)) 
+
+# a='ABCD'
+# print(re.search('[A-I]',a)) 
+
+# a='AVUF'
+# print(re.search('[R-Y]',a)) 
+
+# a='436746'
+# print(re.search('[0-9]',a)) 
+
+# a='436746'
+# print(re.search('[0-3]',a)) 
+
+# a='ads436'
+# print(re.search('[a-z][0-9]',a)) 
+
+# a='abd'
+# print(re.search('[0-9]',a)) 
+
+# a='446'
+# print(re.search('[a-z0-9]',a)) 
+
+# a='abcd'
+# print(re.search('[abcd]',a)) 
+
+# a='abcd'
+# print(re.search('[abcd].',a)) 
+
+# a='abcd'
+# print(re.search('[abcd].*',a)) 
+
+# a='abcd'
+# print(re.search('[abcd].+',a)) 
+
+# a='abcd'
+# print(re.search('[abcd].?',a)) 
