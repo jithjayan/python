7fadab0cf10f025ff7222703f863d7ba1dcd498b
jithjayan <jithjayan9341@gmail.com>
2024-10-19 09:03:15 +0530

    exam patterns

---
+++ exam/pattern.py
@@ -0,0 +1,27 @@
+# a=4
+# for i in range(1,4):
+#     for j in range(1,a):
+#         print(j,end=" ")
+
+#     print()
+#     a-=1
+
+
+# a=65
+# b=4
+# for i in range(1,4):
+#     for j in range(1,b):
+#         print(chr(a),end=" ")
+#         a+=1
+
+#     b-=1    
+#     print()
+    
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         if j%2==0:
+#             print('2',end=" ")
+#         else:
+#             print('1',end=" ")
+#     print()
